8c1b6ca03744d653d33efb37655e0bb1b81aa149
Mitsuo Yoshida <git@ceek.jp>
2013-02-24 03:03:56 +0900

    Delink NonDispBrowser class

---
+++ ContentExtraction/NonDispBrowser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ContentExtraction
+{
+    class NonDispBrowser : WebBrowser
+    {
+        // Reference:
+        //   http://www.atmarkit.co.jp/fdotnet/dotnettips/687nondispbrowser/nondispbrowser.html
+
+        bool done;
+        TimeSpan timeout = new TimeSpan(0, 0, 10);
+
+        protected override void OnDocumentCompleted(WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url == this.Url)
+            {
+                done = true;
+            }
+        }
+
+        protected override void OnNewWindow(CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        public NonDispBrowser()
+        {
+            this.ScriptErrorsSuppressed = true;
+        }
+
+        public bool NavigateAndWait(string url)
+        {
+            base.Navigate(url);
+
+            done = false;
+            DateTime start = DateTime.Now;
+
+            while (done == false)
+            {
+                if (DateTime.Now - start > timeout)
+                {
+                    return false;
+                }
+                Application.DoEvents();
+            }
+            return true;
+        }
+    }
+}
